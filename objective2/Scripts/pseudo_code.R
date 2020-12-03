10160792a27163a014ab2118920cae98b17938b6
jabrams23 <jabrams23@gmail.com>
2020-12-03 10:56:48 +0000

    update

--- objective2/Scripts/pseudo_code.R
+++ objective2/Scripts/pseudo_code.R
@@ -22,6 +22,11 @@ calculate_reward <- function(reward_input, cell){
   return(reward)
 }
 
+calculate_cost <- function(costlayer,prev,cur){
+  tmp_cost <- abs(costlayer[prev]-costlayer[cur])
+  return(tmp_cost)
+}
+
 select_next_cell <- function(cost_layer,cell, transition_mat, covered){
   potential_cells <<- adjacent(cost_layer, cell, directions=8, pairs=FALSE, target=NULL, sorted=FALSE, 
                               include=FALSE, id=FALSE) 
@@ -36,7 +41,6 @@ select_next_cell <- function(cost_layer,cell, transition_mat, covered){
                                             include=FALSE, id=FALSE),
                                size=1)
   }
-  
   if(length(potential_cells)==1) {
     next_cell <- potential_cells
   } else {
@@ -56,10 +60,12 @@ generate_route <- function(start_cell,trans_mat,cost_layer,reward_layer,t){
   current_cell <- start_cell
   cells_covered <- start_cell
   while (total_cost < t){
+    previous_cell <- current_cell
     current_cell <- select_next_cell(cost_layer,current_cell,trans_mat,cells_covered)
     total_reward <- total_reward + calculate_reward(reward_input=reward_layer,
                                                     cell=current_cell)
-    total_cost <- total_cost + cost_layer[current_cell]
+    cost <- calculate_cost(cost_layer,previous_cell,current_cell)
+    total_cost <- total_cost + cost #cost_layer[current_cell]
     cells_covered <- cbind(cells_covered,current_cell)
   }
   tmp_return <- list(cells_covered,total_reward)
@@ -67,11 +73,10 @@ generate_route <- function(start_cell,trans_mat,cost_layer,reward_layer,t){
   #return(total_reward)
 }
 
-
 ce <- function(numb,mat,cost,reward,time) {
   #setup parallel backend to use many processors
   cores=detectCores()
-  cl <- makeCluster(cores[1]-1) #not to overload your computer
+  cl <- makeCluster(cores[1]-2) #not to overload your computer
   registerDoParallel(cl)
   
   rew <- rep(0,numb)
@@ -88,7 +93,7 @@ ce <- function(numb,mat,cost,reward,time) {
   #  route[[i]]<-as.vector(tmp[[1]])
   #}
   
-  tmp_test <- foreach(i = 1:numb, .export=c("calculate_reward", "generate_route","select_next_cell")) %dopar% {
+  tmp_test <- foreach(i = 1:numb, .export=c("calculate_cost","calculate_reward", "generate_route","select_next_cell")) %dopar% {
     print(i)
     tmp <- generate_route(start_cell=1,
                           trans_mat=mat,
@@ -98,6 +103,9 @@ ce <- function(numb,mat,cost,reward,time) {
     #rew[i]<-tmp[[2]]
     #route[[i]]<-as.vector(tmp[[1]])
   }
+  
+  stopCluster(cl)
+  gc()
   for (k in 1:numb) {
     rew[k]<-as.vector(tmp_test[[k]][2])
     route[k]<-as.vector(tmp_test[[k]][1])
@@ -110,11 +118,11 @@ ce <- function(numb,mat,cost,reward,time) {
   return(selected_routes)
 }
 
-update_transition <- function(cost_matrix,tran_mat,select){
+update_transition <- function(cost_matrix,tran_mat,select,numbr){
   tmp1 <- unlist(select)
   tmp3 <- as.data.frame(table(tmp1))
   tran_mat@data@values[!is.na(tran_mat@data@values)] <- 0
-  tran_mat@data@values[tmp3$tmp1] <- as.vector(tmp3$Freq)/numb
+  tran_mat@data@values[tmp3$tmp1] <- as.vector(tmp3$Freq)/(numbr*0.1)
   tran_mat@data@values <- ifelse(tran_mat@data@values>1, 1, tran_mat@data@values)
   return(tran_mat)
 }
@@ -125,26 +133,26 @@ range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 ########## TESTING
 
 
-road_map <- raster(ncol=100, nrow=100)
+road_map <- raster(ncol=10, nrow=10)
 values(road_map) <- 1:ncell(road_map)
 reward_layer <- road_map
-values(reward_layer) <- sample(seq(0,100),10000,replace=TRUE)
+values(reward_layer) <- sample(seq(0,100),100,replace=TRUE)
 cost_layer <- road_map
-values(cost_layer) <- sample(seq(0,10),10000,replace=TRUE)
+values(cost_layer) <- sample(seq(0,10),100,replace=TRUE)
 
 values(cost_layer) <- range01(values(cost_layer))
 
 transition_mat <- cost_layer
-values(transition_mat) <- 0.0
+#values(transition_mat) <- 0.0
 
-numb<-100
-reps<-100
+numb<-1000
+reps<-1000
 for (i in 1:reps) {
-  transition_mat <- (0.5*(1-cost_layer))+(0.5*transition_mat)
-  test <- ce(numb,transition_mat,cost_layer,reward_layer,1000)
-  transition_mat <- update_transition(cost_layer,transition_mat,test)
+  #transition_mat <- (0.5*(1-cost_layer))+(0.5*transition_mat)
+  test <- ce(numb,transition_mat,cost_layer,reward_layer,20)
+  transition_mat <- update_transition(cost_layer,transition_mat,test,numb)
   plot(transition_mat)
 }
 
-tes
+
 
